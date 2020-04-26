599caf0a3c6a8b196b5b37db43e10fed9fb72b92
JuriiTemnokhud <60181144+JuriiTemnokhud@users.noreply.github.com>
2020-04-26 15:32:49 +0300

    Сommit5

--- HTML/Number5/js/index.js
+++ HTML/Number5/js/index.js
@@ -3,8 +3,8 @@
 let userInput;
 
 do {
-  userInput = prompt('Ввуди число 5');
+  userInput = prompt('Введи число 5');
   userInput = Number(userInput);
 } while (userInput !== 5);
 
-alert(`Ну і Дура, "${userInput}"!`);
+alert(`Молодець! "${userInput}"!`);
